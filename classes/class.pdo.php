0e9df9cade4fe7c520a7f21049c30408400791e5
Martin Wegeberg <wegeberg@me.com>
2019-10-22 17:42:45 +0200

    Update class.pdo.php

--- classes/class.pdo.php
+++ classes/class.pdo.php
@@ -1,5 +1,6 @@
 <?php
-	// PDO wrapper class 1.9.7
+	// PDO wrapper class 2.0.0
+	// 2019-10-22 - get_row_join_multi
 	// 2019-09-29 - Formattering af defines
 	// 2019-09-13 - get_result_from_query tilføjet
 	// 2019-08-10 - get_result
@@ -155,13 +156,16 @@
 					$realQuery =
 						"INSERT INTO {$table}
 						({$keysString}) VALUES ({$realValuesString})";
-					echo "\n\n{$query}";
-					echo "\n\n{$realQuery}";
-					exit;
+					// echo "\n\n{$query}";
+					// echo "\n\n{$realQuery}";
+					// exit;
 				}
 				$this->query($query);
 				$this->bindArrayValues($cleanedRow);
 				$this->execute();
+				if($debug) {
+					return $realQuery;
+				}
 				return(true);
 			}
 
@@ -331,6 +335,44 @@
 				return($this->single());
 			}
 
+			public function get_row_join_multi($masterId, $tables, $idFields, $conditions, $select, $order = null, $limit = null, $joinType = "INNER", $isSlug = false) {
+				// ANGIV MASTERTABEL FØRST i $tables og $idFields!!
+				if(!is_array($tables) || !is_array($idFields)) {
+					$this->error = "The first two parameters MUST be arrays!";
+					$this->log_db_error($this->error, "Warning");
+					return([]);
+				}
+				if(empty($tables) || empty($idFields) || count($tables) != count($idFields)) {
+					$this->error = "The two arrays need to be of equal length and not empty!";
+					$this->log_db_error($this->error, "Warning");
+					return([]);
+				}
+				$selectString = $this->make_select($select);
+				if($isSlug) {
+					$conditions[] = "{$tables[0]}.slug LIKE '{$masterId}'";
+				} else {
+					$conditions[] = "{$tables[0]}.{$idFields[0]} = {$masterId}";
+				}
+				$conditionsString = $this->make_conditions($conditions);
+				$joinString = "";
+				for($i = 1; $i < count($tables); $i++) {
+					if(stristr($idFields[$i], "SELECT")) {
+						$joinString .= "{$joinType} JOIN `{$tables[$i]}` ON `{$tables[0]}`.`{$idFields[0]}` = {$idFields[$i]} ";
+					} else {
+						$joinString .= "{$joinType} JOIN `{$tables[$i]}` ON `{$tables[0]}`.`{$idFields[0]}` = `{$tables[$i]}`.`{$idFields[$i]}` ";
+					}
+				}
+				$query =
+					"SELECT {$selectString}
+					FROM `{$tables[0]}`
+					{$joinString}
+					WHERE {$conditionsString} ";
+				if($order) $query .= "ORDER BY {$order} ";
+				if($limit) $query .= "LIMIT {$limit} ";
+				$this->query($query);
+				return($this->single());
+			}
+
 			public function get_distinct($table, $column = "id", $order = null, $conditions = null, $limit = null) {
 				$conditionsString = $this->make_conditions($conditions);
 				// echo $conditionsString;
@@ -356,8 +398,8 @@
 					$query =
 						"SELECT DISTINCT($column)".(!empty($extraFields) ? ", ".implode(", ", $extraFields)." " : "").
 						"FROM {$table}
-						WHERE {$conditionsString} ";
-						"ORDER BY {$order} ";
+						WHERE {$conditionsString}
+						ORDER BY {$order} ";
 						if($limit) $query .= "LIMIT {$limit} ";
 					// echo $query;
 					// exit;
